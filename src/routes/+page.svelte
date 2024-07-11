8be2fed12e0df0f26c8882f21211d1159dd37809
Brad Griffith <brad@buckeyeinnovation.com>
2024-07-11 12:42:19 -0400

    Create a homepage with just the instructions

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import '../css/style.css';
+</script>
+
+<div class="wrapper">
+	<div class="container">
+		<p>Welcome to the Columbus Book Festival <strong>Discovery Quest</strong>!</p>
+		<div style="text-align: left">
+			<h3 class="hint">Instructions:</h3>
+			<div class="hint-container">
+				<ol>
+					<li>Find a card to tap</li>
+					<li>Unlock your phone screen</li>
+					<li>
+						Touch your phone to the tap zone (camera area for iPhone, center back for Android)
+					</li>
+					<li>
+						If you would like to opt out of sharing your information with the booth, tap the "opt
+						out" button on your phone.
+					</li>
+					<li>Repeat!</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+</div>
+
+<footer>
+	<div class="logos">
+		<a href="https://www.columbusbookfestival.org/" target="_blank"
+			><img src="/bookfest-logo.png" class="bookfest-by" alt="Columbus Book Festival" /></a
+		>
+		<a href="https://columbuslibrary.org/foundation/" target="_blank"
+			><img
+				src="/foundation-logo.png"
+				class="foundation-logo"
+				alt="Columbus Metropolitan Library Foundation"
+			/></a
+		>
+	</div>
+	<a href="https://www.buckeyeinnovation.com/" target="_blank"
+		><img src="/tech-by.png" class="powered-by" alt="Technology by Buckeye Innovation" /></a
+	>
+</footer>
